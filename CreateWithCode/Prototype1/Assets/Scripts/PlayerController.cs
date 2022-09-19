8c0058f64d30ff4fd9ac22430058b03158ea1176
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-09-19 09:35:21 -0700

    Update PlayerController.cs

--- CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward
+        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward)
     }
 }
