efe220fb228bd9097a84a7aed0e660323728ade9
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-09-27 10:20:30 -0700

    change

--- CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,6 @@ public class PlayerController : MonoBehaviour
 
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
