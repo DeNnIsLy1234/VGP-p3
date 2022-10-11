1e15c63874c64f4c70e5cb2777c0489246022b4d
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-10-11 10:41:51 -0700

    move side to side

---
+++ CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()   {
+        if (transform.position.x < -10) {
+            transform.position = new Vector3
+        }
+    }
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+    }
+}
