93fd081a031a16ef983d18d21b087cfb1374d390
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-10-26 10:43:56 -0700

    fixes

--- CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/SpawnManager.cs
@@ -7,12 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    private float startDelay = 2;
+    private float spawnDelay = 2;
     private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
