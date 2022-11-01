ee36bc9ed8432e494104af26d813280c61b2aa6c
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-11-01 10:43:05 -0700

    ydychkcvytc

--- CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
+    private float leftLimit = 20;
     private float bottomLimit = -5;
 
     // Update is called once per frame
--- CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float fireRate = 1; // time the player has to wait to fire again
+    private float nextFire = 0; // time since start after which player can fire again
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, if enough time has elapsed since last fire, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate; // reset nextFire to current time + fireRate
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
