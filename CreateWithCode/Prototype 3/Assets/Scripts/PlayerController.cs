5d0e54b5e3a562cbe4f623bf104dbf1fab360600
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-11-16 10:43:31 -0800

    prototye

---
+++ CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
