ce68369f9bdddf3f57ca90d2a57727973524b9f2
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-11-17 10:40:35 -0800

    change

--- CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public jumpForce
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
