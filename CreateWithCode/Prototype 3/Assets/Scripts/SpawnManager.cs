2340c73a0dbf2822510efe8464776d06367efb6c
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-11-28 10:19:06 -0800

    change

--- CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    publicVector3 spawnPos= new Vector3(25, 0, 0);
+    public Vector3 spawnPos= new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
