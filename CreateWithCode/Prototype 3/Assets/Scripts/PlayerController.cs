0a94c5d4adeff5902be271648b0765bc5f03c5b7
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2022-11-29 10:40:13 -0800

    change

--- CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisonEnter(Collision collision)
     {
-        isOnGround = true;
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GameOver");
+            gameOver = true;
+        }
     }
 }
--- CreateWithCode/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
+
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x )
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
     }
 }
