50d48cab4b451a470edaf30c180f4e3a04f852b4
DeNnIsLy1234 <113132309+DeNnIsLy1234@users.noreply.github.com>
2023-02-09 10:40:36 -0800

    stuff

--- CreateWithCode/PersonalProject/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/PersonalProject/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
+    private float speed = 100.0f;
     private Rigidbody playerRb;
     private float zBound = 6;
 
@@ -32,5 +32,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
